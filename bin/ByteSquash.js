d3a44e5a0129caedd336a387012414a31d11e851
Nick Chisiu <nick@squeezer.io>
2019-06-07 17:11:14 +0300

    added main class log support

--- bin/ByteSquash.js
+++ bin/ByteSquash.js
@@ -9,3 +9,4 @@ const CommonYaml = require('../lib/common/yaml');
 const CommonCommand = require('../lib/common/command');
 const CommonCliError = require('../lib/common/cli/error');
 const CommonCliLoader = require('../lib/common/cli/loader');
+const CommonCliLog = require('../lib/common/cli/log');
